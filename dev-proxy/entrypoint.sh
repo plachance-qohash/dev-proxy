990e72ed1d4244a734520fa46f5dfb2b19cb92a7
Patrick Lachance <patrick.lachance@qohash.com>
2024-04-01 11:31:04 -0400

    Support for docker and named pipe commands

--- dev-proxy/entrypoint.sh
+++ dev-proxy/entrypoint.sh
@@ -9,14 +9,15 @@ if [ ! -f "$NAMED_PIPE" ]; then
 fi
 
 extractCert() {
-    if [ ! -f "$DIR/dev-proxy-ca.crt" ]; then
-        openssl pkcs12 -in "$CERT_FILE" -out "$DIR/rootCert.crt" -nodes -passin pass:
-        awk '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/' "$DIR/rootCert.crt" > "$DIR/dev-proxy-ca.crt"
-    fi
+    openssl pkcs12 -in "$CERT_FILE" -out "$DIR/rootCert.crt" -nodes -passin pass:
+    awk '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/' "$DIR/rootCert.crt" > "$DIR/dev-proxy-ca.crt"
 }
 
-if [ ! -d "$DIR" ] || [ ! -f "$CERT_FILE" ]; then
+if [ ! -f "$DIR/dev-proxy-ca.crt" ]; then
     sleep 5 && extractCert &
+fi
+
+if [ ! -d "$DIR" ] || [ ! -f "$CERT_FILE" ]; then
     /app/devproxy --install-cert --ip-address "$CONTAINER_IP" --port 8000
 else
     /app/devproxy --ip-address $CONTAINER_IP --port 8000
